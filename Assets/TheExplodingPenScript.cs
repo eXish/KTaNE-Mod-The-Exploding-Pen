c4d11645a6e653b5404c2b025a37f61d830a2cf2
Brendan King <brendan@brendanking.com>
2020-11-15 23:14:56 -0500

    Quite a few changes
    - Updated text and highlights so they are no longer floating and look good
    - Updated the bundled manual with the latest one on the repo
    - Made it so the message leaver's name is still colored in colorblind mode
    - Made it so the module will not allow interactions while it resets
    - Greatly improved the TP support and added an autosolver
    - Improved the formatting of the logging a bit
    - Removed some useless code
    - The submit button can no longer be pressed after the module is solved
    - Added a press sound to the submit button when everything is correct, and made it so that sound comes from the button and not the solve sound
    - Fixed the colorblind text still being there after the module is solved

--- Assets/TheExplodingPenScript.cs
+++ Assets/TheExplodingPenScript.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Linq;
 using UnityEngine;
@@ -16,6 +15,7 @@ public class TheExplodingPenScript : MonoBehaviour {
 
 	public MeshRenderer PenRender;
 	public MeshRenderer NotepadRender;
+    public TextMesh[] ColorblindText;
 
 	private int NumIndicators = 0;
 	private int LastSerialDigit = 0;
@@ -28,6 +28,7 @@ public class TheExplodingPenScript : MonoBehaviour {
 	private int MessageColorIndex = 0;
 
 	private bool ColorBlindEnabled;
+    private bool Waiting;
 
 	private static Color[] Colors = new Color[]{
 
@@ -112,33 +113,94 @@ public class TheExplodingPenScript : MonoBehaviour {
 
 	
 	//Twitch Plays Support.
-	private string TwitchHelpMessage = "Press the pen with !{0} <seconds digets>. Press the submit button with !{0} submit. Enable colorblind with !{0} colorblind.";
+	private string TwitchHelpMessage = "Press the pen with !{0} pen <seconds digits> <times>. Press the submit button with !{0} submit. Toggle colorblind mode with !{0} colorblind.";
 	public IEnumerator ProcessTwitchCommand(string command)
 	{
-			command = command.ToUpper();
-			if (command.Contains("PEN"))
-			{
-				string TwitchCommand = command.TrimStart('P', 'E', 'N', ' ');
-				int Seconds = System.Int16.Parse(TwitchCommand);
-				while(Mathf.FloorToInt(Info.GetTime())%60 != Seconds) yield return "trycancel Pen wasn't pressed due to request to cancel";
-				yield return new KMSelectable[] {Pen};
-			}
+        if (Waiting)
+        {
+            yield return "sendtochaterror Cannot interact with the module while it is resetting.";
+            yield break;
+        }
+        string tpcommand = command.ToUpper();
+        if (tpcommand.StartsWith("PEN ") && tpcommand.Split(' ').Length == 3)
+        {
+            string[] TwitchCommands = tpcommand.Substring(4).Split(' ');
+            int Seconds = -1;
+            if (!int.TryParse(TwitchCommands[0], out Seconds) || TwitchCommands[0].Length != 2)
+            {
+                yield return "sendtochaterror The seconds digits '" + command.Split(' ')[1] + "' are invalid!";
+                yield break;
+            }
+            if (Seconds < 0 || Seconds > 59)
+            {
+                yield return "sendtochaterror The seconds digits '" + command.Split(' ')[1] + "' are invalid!";
+                yield break;
+            }
+            int Times = -1;
+            if (!int.TryParse(TwitchCommands[1], out Times))
+            {
+                yield return "sendtochaterror The number of times '" + command.Split(' ')[2] + "' is invalid!";
+                yield break;
+            }
+            if (Times < 1 || Times > 10)
+            {
+                yield return "sendtochaterror The number of times '" + command.Split(' ')[2] + "' is invalid!";
+                yield break;
+            }
+            while (Mathf.FloorToInt(Info.GetTime()) % 60 != Seconds) yield return "trycancel The pen wasn't pressed due to a request to cancel.";
+            for (int i = 0; i < Times; i++)
+            {
+                if (Mathf.FloorToInt(Info.GetTime()) % 60 != Seconds)
+                {
+                    yield return "sendtochat Halted pressing the pen due to a change in the seconds digits of the bomb timer. Successfully pressed the pen " + i + " times before halting.";
+                }
+                Pen.OnInteract();
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
 
-			if (command.Contains("SUBMIT"))
-			{
-				yield return new KMSelectable[] {Submit};
-			}
+        if (tpcommand.Equals("SUBMIT"))
+        {
+            yield return null;
+            Submit.OnInteract();
+        }
 
-			if (command.Contains("COLORBLIND"))
-			{
-				ColorBlindEnabled = true;
-				ColorblindOn();
-			}
-	}
+        if (tpcommand.Equals("COLORBLIND") || tpcommand.Equals("COLOURBLIND"))
+        {
+            yield return null;
+            if (ColorBlindEnabled == true)
+            {
+                ColorBlindEnabled = false;
+                ColorblindText[0].text = "";
+                ColorblindText[1].text = "";
+            }
+            else
+            {
+                ColorBlindEnabled = true;
+                ColorblindOn();
+            }
+        }
+    }
+
+    public IEnumerator TwitchHandleForcedSolve()
+    {
+        if (PenClicks > AllowedPenClicks)
+            PenClicks = 0;
+        int times = AllowedPenClicks - PenClicks;
+        while (Waiting) yield return true;
+        for (int i = 0; i < times; i++)
+        {
+            while (Mathf.FloorToInt(Info.GetTime()) % 60 != AllowedTime) yield return true;
+            Pen.OnInteract();
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return null;
+        Submit.OnInteract();
+    }
 
 	void Start () {
 		//Colorblind Support
-		if (GetComponent<KMColorblindMode>().ColorblindModeActive == true)
+		if (ColorblindMode.ColorblindModeActive == true)
 		{
 			ColorBlindEnabled = true;
 			ColorblindOn();
@@ -153,12 +215,12 @@ public class TheExplodingPenScript : MonoBehaviour {
 		}
 
 		if (LastSerialDigit == 0){
-		LastSerialDigit = 10;
+		    LastSerialDigit = 10;
 		}
 
 		//Calculate number of clicks.
 		AllowedPenClicks = ((LastSerialDigit * NumIndicators) % 10) + 1;
-		Debug.LogFormat("[The Exploding Pen #{0}] Expected number of clicks: " + AllowedPenClicks, moduleId);
+        Debug.LogFormat("[The Exploding Pen #{0}] Expected number of clicks: {1}", moduleId, AllowedPenClicks);
 
 		//Debug.LogFormat("[The Exploding Pen #{0}] DEBUG: NumIndicators: " + NumIndicators, moduleId); //Debug Code
 		//Debug.LogFormat("[The Exploding Pen #{0}] DEBUG: LastSerialDigit: " + LastSerialDigit, moduleId); //Debug Code
@@ -172,19 +234,19 @@ public class TheExplodingPenScript : MonoBehaviour {
 	void GenerateSolution()
 	{
 		//Visual Setup
-		PenColorIndex = UnityEngine.Random.Range(0, Colors.Length);
+		PenColorIndex = Random.Range(0, Colors.Length);
 		PenRender.material.color = Colors[PenColorIndex];
 
-		MessageColorIndex = UnityEngine.Random.Range(0, Colors.Length);
+		MessageColorIndex = Random.Range(0, Colors.Length);
 		NotepadRender.GetComponentInChildren<TextMesh>().color = Colors[MessageColorIndex];
 
-		MessageTextIndex = UnityEngine.Random.Range(0, MessageText.Length);
+		MessageTextIndex = Random.Range(0, MessageText.Length);
 		NotepadRender.GetComponentInChildren<TextMesh>().text = MessageText[MessageTextIndex];
 
 		//Visual Setup Logging
-		Debug.LogFormat("[The Exploding Pen #{0}] The pen color is, " +  ColorsNames[PenColorIndex] + ".", moduleId);
-		Debug.LogFormat("[The Exploding Pen #{0}] The ink color is, " +  ColorsNames[MessageColorIndex] + ".", moduleId);
-		Debug.LogFormat("[The Exploding Pen #{0}] The the person who called you was, " +  MessageText[MessageTextIndex] + ".", moduleId);
+		Debug.LogFormat("[The Exploding Pen #{0}] The pen color is {1}.", moduleId, ColorsNames[PenColorIndex]);
+		Debug.LogFormat("[The Exploding Pen #{0}] The ink color is {1}.", moduleId, ColorsNames[MessageColorIndex]);
+		Debug.LogFormat("[The Exploding Pen #{0}] The person who left you the voice message was {1}.", moduleId, MessageText[MessageTextIndex]);
 
 		//Submit Time Setup
 		if(MessageText[MessageTextIndex] == "Danielstigman"){
@@ -218,18 +280,19 @@ public class TheExplodingPenScript : MonoBehaviour {
 		}
 
 		ColorblindOn();
+        Waiting = false;
 	}
 
 
 	void ColorblindOn(){
 		if (ColorBlindEnabled == true)
 		{
-			NotepadRender.GetComponentInChildren<TextMesh>().text = "Pen: " + ColorsNames[PenColorIndex] + "\n" + MessageText[MessageTextIndex] + "\n Ink: " + ColorsNames[MessageColorIndex];
-			NotepadRender.GetComponentInChildren<TextMesh>().color = Color.black;
-		}
+			ColorblindText[0].text = "Pen: " + ColorsNames[PenColorIndex];
+            ColorblindText[1].text = "Ink: " + ColorsNames[MessageColorIndex];
+        }
 	}
 	void PressedPen(){
-		if (moduleSolved == false)
+		if (moduleSolved == false && !Waiting)
 		{
 			int TimePressed = (int) Info.GetTime() % 60;
 			//Debug.LogFormat("[The Exploding Pen #{0}] DEBUG: TimePressed XX:" + TimePressed, moduleId); //Debug Code.
@@ -242,12 +305,12 @@ public class TheExplodingPenScript : MonoBehaviour {
 					
 					if (TimePressed < 10)
 					{
-						Debug.LogFormat("[The Exploding Pen #{0}] You pressed the pen at XX:" + "0" + TimePressed + ". Expected time XX:" + AllowedTime + " You have clicked the pen " + PenClicks + " times.", moduleId);
+						Debug.LogFormat("[The Exploding Pen #{0}] You pressed the pen at XX:0{1}. Expected time XX:{2}. You have clicked the pen {3} times.", moduleId, TimePressed, AllowedTime.ToString("00"), PenClicks);
 					}
 					
 					else
 					{
-						Debug.LogFormat("[The Exploding Pen #{0}] You pressed the pen at XX:" + TimePressed + ". Expected time XX:" + AllowedTime + " You have clicked the pen " + PenClicks + " times.", moduleId);
+						Debug.LogFormat("[The Exploding Pen #{0}] You pressed the pen at XX:{1}. Expected time XX:{2}. You have clicked the pen {3} times.", moduleId, TimePressed, AllowedTime.ToString("00"), PenClicks);
 					}
 					
 				}
@@ -258,45 +321,67 @@ public class TheExplodingPenScript : MonoBehaviour {
 					Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, Pen.transform);
 					Module.HandleStrike();
 					NotepadRender.GetComponentInChildren<TextMesh>().text = "Module reset.";
+                    if (ColorBlindEnabled == true)
+                    {
+                        ColorblindText[0].text = "";
+                        ColorblindText[1].text = "";
+                    }
 					NotepadRender.GetComponentInChildren<TextMesh>().color = new Color32(0, 160, 0, 255); // Green
+                    Waiting = true;
 					Invoke("GenerateSolution", 2);
 
-					if (TimePressed < 10)
-					{
-						Debug.LogFormat("[The Exploding Pen #{0}] You pressed the pen at XX:" + "0" + TimePressed + ". Expected time XX:" + AllowedTime + " Strike! Module reset.", moduleId);
-					}
-					
-					else
-					{
-						Debug.LogFormat("[The Exploding Pen #{0}] You pressed the pen at XX:" + TimePressed + ". Expected time XX:" + AllowedTime + " Strike! Module reset.", moduleId);
-					}
-					
-				}
+                if (TimePressed < 10)
+                {
+                    Debug.LogFormat("[The Exploding Pen #{0}] You pressed the pen at XX:0{1}. Expected time XX:{2}. Strike! Module reset.", moduleId, TimePressed, AllowedTime.ToString("00"));
+                }
+
+                else
+                {
+                    Debug.LogFormat("[The Exploding Pen #{0}] You pressed the pen at XX:{1}. Expected time XX:{2}. Strike! Module reset.", moduleId, TimePressed, AllowedTime.ToString("00"));
+                }
+
+            }
 	}
 		}
 
 	void PressedSubmit(){
-		if (AllowedPenClicks == PenClicks && PenClicks > 0) {
-			Debug.LogFormat("[The Exploding Pen #{0}] You pressed the submit button. Expected number of clicks: " + AllowedPenClicks + " You clicked the pen " + PenClicks + " times. GG Module solved!", moduleId);
-			Submit.AddInteractionPunch();
-			Module.HandlePass();
-			Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.CorrectChime, Submit.transform);
-			NotepadRender.GetComponentInChildren<TextMesh>().text = "GG";
-			NotepadRender.GetComponentInChildren<TextMesh>().color = new Color32(0, 160, 0, 255);
-			PenRender.material.color = new Color32(0, 160, 0, 255);
-			moduleSolved = true;
-		}
+        if (moduleSolved == false && !Waiting)
+        {
+            if (AllowedPenClicks == PenClicks && PenClicks > 0)
+            {
+                Debug.LogFormat("[The Exploding Pen #{0}] You pressed the submit button. Expected number of clicks: {1}. You clicked the pen {2} times. GG Module solved!", moduleId, AllowedPenClicks, PenClicks);
+                Submit.AddInteractionPunch();
+                Module.HandlePass();
+                Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, Submit.transform);
+                Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.CorrectChime, transform);
+                NotepadRender.GetComponentInChildren<TextMesh>().text = "GG";
+                if (ColorBlindEnabled == true)
+                {
+                    ColorblindText[0].text = "";
+                    ColorblindText[1].text = "";
+                }
+                NotepadRender.GetComponentInChildren<TextMesh>().color = new Color32(0, 160, 0, 255);
+                PenRender.material.color = new Color32(0, 160, 0, 255);
+                moduleSolved = true;
+            }
 
-		else
-		{
-			Debug.LogFormat("[The Exploding Pen #{0}] You pressed the submit button. Expected number of clicks: " + AllowedPenClicks + " You clicked the pen " + PenClicks + " times. Strike! Module reset", moduleId);
-			PenClicks = 0;
-			Submit.AddInteractionPunch();
-			Module.HandleStrike();
-			Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, Submit.transform);
-			NotepadRender.GetComponentInChildren<TextMesh>().text = "Module reset.";
-			NotepadRender.GetComponentInChildren<TextMesh>().color = new Color32(0, 160, 0, 255);
-			Invoke("GenerateSolution", 2);
-		}
+            else
+            {
+                Debug.LogFormat("[The Exploding Pen #{0}] You pressed the submit button. Expected number of clicks: {1}. You clicked the pen {2} times. Strike! Module reset", moduleId, AllowedPenClicks, PenClicks);
+                PenClicks = 0;
+                Submit.AddInteractionPunch();
+                Module.HandleStrike();
+                Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, Submit.transform);
+                NotepadRender.GetComponentInChildren<TextMesh>().text = "Module reset.";
+                if (ColorBlindEnabled == true)
+                {
+                    ColorblindText[0].text = "";
+                    ColorblindText[1].text = "";
+                }
+                NotepadRender.GetComponentInChildren<TextMesh>().color = new Color32(0, 160, 0, 255);
+                Waiting = true;
+                Invoke("GenerateSolution", 2);
+            }
+        }
 	}
 }
